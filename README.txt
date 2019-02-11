4cda7a558b5518b4c56d31e537e3c134c0515f2a
Jennifer-Seery <jennifermarieseery15@gmail.com>
2019-02-11 15:28:31 +0000

    Code Exersize

---
+++ README.txt
@@ -0,0 +1,6 @@
+Note: Sorry guys! Work was super busy over the weekend and I didnt get a chance to finish it. I only really could spare a day of work due to how busy I was. I tried to add an example of each different area present just to display what I know. For example, I used gulp, ejb, npm, scss, javascript, working with json etc.
+
+If I had more time, I would fix the appearance of the GUI more CSS add in the images. Improve filtering and make it more efficent. Implement filtering of years and add in the testimonials. 
+
+
+The GUI can be seen by running the 'gulp' command. All needed files get added into the 'build' directory.
--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,39 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var concat = require('gulp-concat');
 var ejs = require('gulp-ejs');
+var browserSync = require('browser-sync').create();
 
-gulp.task('css', ['clean:css'], function() {
-
+gulp.task('css', function() {
+	return gulp.src('src/scss/styles.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('build/pages'))
 });
 
-gulp.task('js', ['clean:js'], function() {
+gulp.task('js', function() {
+  return gulp.src('src/js/**')
+   .pipe(gulp.dest('build/pages'))
+});
 
+gulp.task('data', function() {
+  return gulp.src('src/js/data/**')
+   .pipe(gulp.dest('build/pages'))
 });
 
 gulp.task('ejs', function() {
-
-});
-
-gulp.task('html', function() {
-
+  return gulp.src('src/views/**/*.ejs')
+   .pipe(ejs({}, {ext:'.html'}))
+   .pipe(gulp.dest('build'))
 });
 
 gulp.task('serve', function() {
 
+ browserSync.init({
+        server: "./build/pages/"  
+    });
+
 });
 
 // The default task (called when we run `gulp` from cli)
-gulp.task('default', ['ejs', 'js', 'sass'], function() {
+gulp.task('default', [ 'css', 'js', 'data', 'ejs','serve'], function() {
 
 });
